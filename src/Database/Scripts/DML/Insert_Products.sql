9d80947d40b0de4b141993682071ea01ba90bba2
mdigit <mdigit@gmail.com>
2016-10-22 11:23:21 +0200

    Added DML

---
+++ src/Database/Scripts/DML/Insert_Products.sql
@@ -0,0 +1,13 @@
+﻿DELETE Products
+
+INSERT INTO Products (Name, Description, Catagory, Price) VALUES
+('Kayak', 'A boat for one person', 'Watersports', 275.00 ),
+('Lifejacket', 'Protective and fashionable', 'Watersports', 48.95 ),
+('Soccer Ball', 'FIFA-approved size and weight', 'Soccer', 19.50 ),
+('Corner Flags', 'Give your playing field a professional touch', 'Soccer', 34.95 ),
+('Stadium', 'Flat-packed 35,000-seat stadium', 'Soccer', 79500.00 ),
+('Thinking Cap', 'Improve your brain efficiency by 75%', 'Chess', 16.00 ),
+('Unsteady Chair', 'Secretly give your opponent a disadvantage', 'Chess', 29.95 ),
+('Thinking Cap', 'Improve your brain efficiency by 75%', 'Chess', 275.00 ),
+('Human Chess Board', 'A fun game for the family', 'Chess', 75.00 ),
+('Bling-Bling King', 'Gold-plated, diamond-studded King', 'Chess', 1200.00 )
